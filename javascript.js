bf21a9111e0312a52d6a5c6e444fc12274ace84d
megosaint <megosaint@gmail.com>
2022-03-30 13:46:30 +0300

    Add UI

--- javascript.js
+++ javascript.js
@@ -6,38 +6,39 @@ function computerPlay(){
 
 
 function playRound(playerSelection, computerSelection){
+    const round = document.querySelector('.round');
     if (playerSelection.toLowerCase() === computerSelection){
-        console.log('Tie!');
+        round.textContent = 'Tie!';
         return 2;
     }
 
     if (playerSelection.toLowerCase() === 'rock'){
         if (computerSelection === 'paper'){
-            console.log('You lose! Paper beats Rock');
+            round.textContent = 'You lose! Paper beats Rock';
             return 0;
         }
         else{
-            console.log('You win! Rock beats Scissor');
+            round.textContent = 'You win! Rock beats Scissor';
             return 1;
         }
     }
     if (playerSelection.toLowerCase() === 'paper'){
         if (computerSelection === 'scissor'){
-            console.log('You lose! Scissor beats Paper');
+            round.textContent = 'You lose! Scissor beats Paper';
             return 0;
         }
         else{
-            console.log('You win! Paper beats Rock');
+            round.textContent = 'You win! Paper beats Rock';
             return 1;
         }
     }
     if (playerSelection.toLowerCase() === 'scissor'){
         if (computerSelection === 'rock'){
-            console.log('You lose! Rock beats Scissor');
+            round.textContent = 'You lose! Rock beats Scissor';
             return 0;
         }
         else{
-            console.log('You win! Scissor beats Paper');
+            round.textContent = 'You win! Scissor beats Paper';
             return 1;
         }
     }
@@ -46,26 +47,36 @@ function playRound(playerSelection, computerSelection){
 function game(){
     let playerCount = 0;
     let computerCount = 0;
-    while(playerCount !== 5 && computerCount !== 5){
-        let playerSelection = prompt('Your choise?');
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-        if (result === 0){
-            computerCount ++;
-        }
-        else if (result === 1){
-            playerCount ++;
-        }
+    let result = 0;
+    const btn = document.querySelectorAll('button');
+    const playerScore = document.querySelector('.player-score');
+    const computerScore = document.querySelector('.computer-score');
+    const results = document.querySelector('#results');
+    const p = document.createElement('p');
+    btn.forEach((button) => {
+    
+        button.addEventListener('click', () => {
+            result = playRound(button.className, computerPlay());
+            if (result === 0){
+                computerCount ++;
+            }
+            else if (result === 1){
+                playerCount ++;
+            }
+            playerScore.textContent = `Player Score: ${playerCount}`;
+            computerScore.textContent = `Computer Score: ${computerCount}`;
+            if (playerCount === 5){
+                results.appendChild(p);
+                p.textContent = 'YOU ARE WINNER';
+            }
+            else if (computerCount === 5){
+                results.appendChild(p);
+                p.textContent = 'COMPUTER ARE WINNER';
+            }
+        });
+
+    });      
         result = 0;
-        console.log('Player: ' + playerCount);
-        console.log('Computer: ' + computerCount);
-    }
-    if (playerCount === 5){
-        console.log('YOU ARE WINNER');
-    }
-    else{
-        console.log('COMPUTER IS WINNER');
-    }
 }
 
 game();
